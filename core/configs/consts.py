21b5e5a3c468d05388ff65b6d107910802b846b5
Artem Batalov <a@batalov.me>
2021-11-26 20:24:44 +0000

    changes from server

--- core/configs/consts.py
+++ core/configs/consts.py
@@ -11,10 +11,11 @@ BASE_DIR: Path = Path().cwd()
 LOGS_FOLDER: Path = BASE_DIR / "logs"
 
 base_dates_campus_cleaning = {
-    1: [date(2019, 4, 19), None, date(2019, 4, 29), date(2019, 5, 8)],
-    2: [date(2019, 4, 15), date(2019, 4, 24), date(2019, 5, 3), None],
-    3: [date(2019, 4, 17), date(2019, 4, 26), None, date(2019, 5, 6)],
-    4: [None, date(2019, 4, 22), date(2019, 5, 1), date(2019, 5, 10)],
+    1: [date(2020, 8, 31), date(2020, 9, 9), date(2020, 9, 18), None],
+    '2 (1-2)': [date(2020, 9, 4), None, date(2020, 9, 14), date(2020, 9, 23)],
+    '2 (3-4)': [date(2020, 9, 4), None, date(2020, 9, 14), date(2020, 9, 23)],
+    3: [None, date(2020, 9, 7), date(2020, 9, 16), date(2020, 9, 25)],
+    4: [date(2020, 9, 2), date(2020, 9, 11), None, date(2020, 9, 21)],
 }  # campus_number -> some day with cleaning
 cleaning_interval = 28
 default_timezone = pytz.timezone("Europe/Moscow")
